aeaf607c7868103c4c6ff299f2ec04bf2dbf406a
Kamil Toporek <kamil.toporek98@gmail.com>
2024-03-27 15:33:38 +0100

    Assigning variables

--- kalkulator.py
+++ kalkulator.py
@@ -1,3 +1,3 @@
-int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
-int(input("Wprowadź pierwszą liczbę: "))
-int(input("Wprowadź drugą liczbę: "))    
+operator = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+a = int(input("Wprowadź pierwszą liczbę: "))
+b = int(input("Wprowadź drugą liczbę: "))    
