38172f7501cc009ab3d7898f1a98968cb821fde4
KenZooo11 <164327728+KenZooo11@users.noreply.github.com>
2024-03-27 19:15:47 +0100

    Adding function to kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -5,18 +5,31 @@ operator = int(input("Podaj działanie, posługując się odpowiednią liczbą: 
 a = float(input("Wprowadź pierwszą liczbę: "))
 b = float(input("Wprowadź drugą liczbę: "))    
 
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+
 if operator == 1:
     logging.info(f"Dodaję {a} i {b}")
-    print(f"Wynik wynosi ", a + b)
+    print(f"Wynik wynosi ", add(a, b))
 elif operator == 2:
     logging.info(f"Odejmuję {a} i {b}")
-    print(f"Wynik wynosi ", a - b)
+    print(f"Wynik wynosi ", subtract(a, b))
 elif operator == 3:
     logging.info(f"Mnożę {a} i {b}")
-    print(f"Wynik wynosi ", a * b)
+    print(f"Wynik wynosi ", multiply(a, b))
 elif operator == 4:
     logging.info(f"Dzielę {a} i {b}")
     if b == 0:
         logging.error("NIE MOŻNA DZIELIĆ PRZEZ ZERO!")
     else:
-        print(f"Wynik wynosi ", a / b)
+        print(f"Wynik wynosi ", divide(a, b))
