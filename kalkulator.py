9df08b7d38482b5bf6c6d19f92fcae9f7b574809
Kamil Toporek <kamil.toporek98@gmail.com>
2024-03-27 15:53:58 +0100

    Adding logging info

--- kalkulator.py
+++ kalkulator.py
@@ -1,3 +1,20 @@
+import logging
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 operator = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
-a = int(input("Wprowadź pierwszą liczbę: "))
-b = int(input("Wprowadź drugą liczbę: "))    
+a = float(input("Wprowadź pierwszą liczbę: "))
+b = float(input("Wprowadź drugą liczbę: "))    
+
+if operator == 1:
+    logging.info(f"Dodaję {a} i {b}")
+    print(f"Wynik wynosi ", a + b)
+elif operator == 2:
+    logging.info(f"Odejmuję {a} i {b}")
+    print(f"Wynik wynosi ", a - b)
+elif operator == 3:
+    logging.info(f"Mnożę {a} i {b}")
+    print(f"Wynik wynosi ", a * b)
+elif operator == 4:
+    logging.info(f"Dzielę {a} i {b}")
+    if b == 0:
+        logging.error("Nie można dzielić przez zero!")
+    print(f"Wynik wynosi ", a / b)
